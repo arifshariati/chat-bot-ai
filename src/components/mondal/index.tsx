9a1e5232076da1a14fd21d231723467c07606b49
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-06-13 15:29:58 +0200

    added modal component

---
+++ src/components/mondal/index.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
+
+type ModalProps = {
+  trigger: React.ReactNode;
+  children: React.ReactNode;
+  title: string;
+  description: string;
+  type?: "Integration";
+  logo?: string;
+};
+
+const Modal = ({ trigger, children, title, description, type, logo }: ModalProps) => {
+  switch (type) {
+    case "Integration":
+      return (
+        <Dialog>
+          <DialogTrigger asChild>{trigger}</DialogTrigger>
+          <DialogContent>
+            <div className="flex justify-center gap-3">
+              <div className="w-12 h-12 relative">
+                <Image src={`https://ucarecdn.com/2c9bd4ab-1f00-41df-bad2-df668f65a232/`} fill alt="Corinna" />
+              </div>
+              <div className="text-gray-400">
+                <ArrowLeft size={20} />
+                <ArrowRight size={20} />
+              </div>
+              <div className="w-12 h-12 relative">
+                <Image src={`https://ucarecdn.com/${logo}/`} fill alt="Stripe" />
+              </div>
+            </div>
+            <DialogHeader className="flex items-center">
+              <DialogTitle className="text-xl">{title}</DialogTitle>
+              <DialogDescription className=" text-center">{description}</DialogDescription>
+            </DialogHeader>
+            {children}
+          </DialogContent>
+        </Dialog>
+      );
+    default:
+      return (
+        <Dialog>
+          <DialogTrigger asChild>{trigger}</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="text-xl">{title}</DialogTitle>
+              <DialogDescription>{description}</DialogDescription>
+            </DialogHeader>
+            {children}
+          </DialogContent>
+        </Dialog>
+      );
+  }
+};
+
+export default Modal;

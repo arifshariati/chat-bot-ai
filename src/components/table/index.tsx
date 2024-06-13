c004995f664128913ca05348f21bed76b8baec55
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-06-13 16:05:40 +0200

    added shadcn table and custom table component

---
+++ src/components/table/index.tsx
@@ -0,0 +1,24 @@
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { cn } from "@/lib/utils";
+
+type DataTableProps = {
+  headers: string[];
+  children: React.ReactNode;
+};
+
+export const DataTable = ({ headers, children }: DataTableProps) => {
+  return (
+    <Table className="rounded-t-xl overflow-hidden">
+      <TableHeader>
+        <TableRow className="">
+          {headers.map((header, key) => (
+            <TableHead key={key} className={cn(key == headers.length - 1 && "text-right", "text-black")}>
+              {header}
+            </TableHead>
+          ))}
+        </TableRow>
+      </TableHeader>
+      <TableBody>{children}</TableBody>
+    </Table>
+  );
+};

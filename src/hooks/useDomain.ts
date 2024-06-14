bc63b688ce0be0d538df57aae110a2dc4ca7de68
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-06-14 13:01:35 +0200

    uncommented missing codes in useDomain hook

--- src/hooks/useDomain.ts
+++ src/hooks/useDomain.ts
@@ -2,15 +2,15 @@ import { onIntegrateDomain } from "@/actions/settings";
 import { useToast } from "@/components/ui/use-toast";
 import { AddDomainSchema } from "@/schemas/settings.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
-// import { UploadClient } from "@uploadcare/upload-client";
+import { UploadClient } from "@uploadcare/upload-client";
 import { usePathname, useRouter } from "next/navigation";
 
 import { useEffect, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
-// const upload = new UploadClient({
-//   publicKey: process.env.NEXT_PUBLIC_UPLOAD_CARE_PUBLIC_KEY as string,
-// });
+const upload = new UploadClient({
+  publicKey: process.env.NEXT_PUBLIC_UPLOAD_CARE_PUBLIC_KEY as string,
+});
 
 export const useDomain = () => {
   const {
@@ -32,24 +32,24 @@ export const useDomain = () => {
     setIsDomain(pathname.split("/").pop());
   }, [pathname]);
 
-  //   const onAddDomain = handleSubmit(async (values: FieldValues) => {
-  //     setLoading(true);
-  //     const uploaded = await upload.uploadFile(values.image[0]);
-  //     const domain = await onIntegrateDomain(values.domain, uploaded.uuid);
-  //     if (domain) {
-  //       reset();
-  //       setLoading(false);
-  //       toast({
-  //         title: domain.status == 200 ? "Success" : "Error",
-  //         description: domain.message,
-  //       });
-  //       router.refresh();
-  //     }
-  //   });
+  const onAddDomain = handleSubmit(async (values: FieldValues) => {
+    setLoading(true);
+    const uploaded = await upload.uploadFile(values.image[0]);
+    const domain = await onIntegrateDomain(values.domain, uploaded.uuid);
+    if (domain) {
+      reset();
+      setLoading(false);
+      toast({
+        title: domain.status == 200 ? "Success" : "Error",
+        description: domain.message,
+      });
+      router.refresh();
+    }
+  });
 
   return {
     register,
-    // onAddDomain,
+    onAddDomain,
     errors,
     loading,
     isDomain,

a08dd054add722f4d54685f28f4c30376f44c06b
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-06-14 13:07:17 +0200

    refactor useDomain hook

--- src/hooks/useDomain.ts
+++ src/hooks/useDomain.ts
@@ -1,12 +1,11 @@
+import { useEffect, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { usePathname, useRouter } from "next/navigation";
+import { UploadClient } from "@uploadcare/upload-client";
 import { onIntegrateDomain } from "@/actions/settings";
 import { useToast } from "@/components/ui/use-toast";
 import { AddDomainSchema } from "@/schemas/settings.schema";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { UploadClient } from "@uploadcare/upload-client";
-import { usePathname, useRouter } from "next/navigation";
-
-import { useEffect, useState } from "react";
-import { FieldValues, useForm } from "react-hook-form";
 
 const upload = new UploadClient({
   publicKey: process.env.NEXT_PUBLIC_UPLOAD_CARE_PUBLIC_KEY as string,

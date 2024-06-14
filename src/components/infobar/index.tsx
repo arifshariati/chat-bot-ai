dbd11e8ef2e92e2835bab2a553e2e9cbe1ad12ef
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-06-14 13:43:45 +0200

    refactored infobar component

--- src/components/infobar/index.tsx
+++ src/components/infobar/index.tsx
@@ -9,13 +9,13 @@ const InfoBar = () => {
       <BreadCrumb />
       <div className="flex gap-3 items-center">
         <div>
-          <Card className="rounded-xl flex gap-3 py-3 px-4 text-ghost">
+          <Card className="rounded-full flex gap-3 py-2 px-5 text-ghost">
             <Trash />
             <Star></Star>
           </Card>
         </div>
         <Avatar>
-          <AvatarFallback className="bg-orange text-white">
+          <AvatarFallback className="bg-secondary text-primary">
             <Headphones />
           </AvatarFallback>
         </Avatar>

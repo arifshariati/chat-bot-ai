dee7d39cdc3c46ce09ba28211751e12dea289dca
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-06-17 12:56:42 +0200

    added cards components for dashboard page

---
+++ src/components/dashboard/cards.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type DashboardCardProps = {
+  title: string;
+  value: number;
+  icon: JSX.Element;
+  sales?: boolean;
+};
+
+export const DashboardCard = ({ icon, title, value, sales }: DashboardCardProps) => {
+  return (
+    <div className=" rounded-lg flex flex-col gap-3 pr-10 pl-10 py-10 md:pl-10 md:pr-20 border-[1px] border-border bg-cream dark:bg-muted md:w-fit w-full">
+      <div className="flex gap-3">
+        {icon}
+        <h2 className="font-bold text-xl">{title}</h2>
+      </div>
+      <p className="font-bold text-4xl">
+        {sales && "$"}
+        {value}
+      </p>
+    </div>
+  );
+};

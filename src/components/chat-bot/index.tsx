53bd6056cd286f919031a11ca2cde31b2cf471bb
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-06-18 14:58:18 +0200

    added chat bot component

---
+++ src/components/chat-bot/index.tsx
@@ -0,0 +1,56 @@
+"use client";
+import Image from "next/image";
+import { useChatBot } from "@/hooks/chatbot/use-chatbot";
+import { BotWindow } from "./window";
+import { cn } from "@/lib/utils";
+import { BotIcon } from "@/icons/bot-icon";
+
+const AiChatBot = () => {
+  const {
+    onOpenChatBot,
+    botOpened,
+    onChats,
+    register,
+    onStartChatting,
+    onAiTyping,
+    messageWindowRef,
+    currentBot,
+    loading,
+    onRealTime,
+    setOnChats,
+    errors,
+  } = useChatBot();
+
+  return (
+    <div className="h-screen flex flex-col justify-end items-end gap-4">
+      {botOpened && (
+        <BotWindow
+          errors={errors}
+          setChat={setOnChats}
+          realtimeMode={onRealTime}
+          helpdesk={currentBot?.helpdesk!}
+          domainName={currentBot?.name!}
+          ref={messageWindowRef}
+          help={currentBot?.chatBot?.helpdesk}
+          theme={currentBot?.chatBot?.background}
+          textColor={currentBot?.chatBot?.textColor}
+          chats={onChats}
+          register={register}
+          onChat={onStartChatting}
+          onResponding={onAiTyping}
+        />
+      )}
+      <div
+        className={cn(
+          "rounded-full relative cursor-pointer shadow-md w-20 h-20 flex items-center justify-center bg-grandis",
+          loading ? "invisible" : "visible"
+        )}
+        onClick={onOpenChatBot}
+      >
+        {currentBot?.chatBot?.icon ? <Image src={`https://ucarecdn.com/${currentBot.chatBot.icon}/`} alt="bot" fill /> : <BotIcon />}
+      </div>
+    </div>
+  );
+};
+
+export default AiChatBot;
---
+++ src/components/chat-bot/responding.tsx
@@ -0,0 +1,19 @@
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+
+export const Responding = () => {
+  return (
+    <div className="self-start flex items-end gap-3">
+      <Avatar className="w-5 h-5">
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <div className="chat-bubble">
+        <div className="typing">
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
